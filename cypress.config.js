68b9722d280aadf96b7b507406e53c5c32bef8e0
Fabrizio Otranto <Fabryotranto@gmail.com>
2022-11-17 12:50:09 -0300

    a

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
-
+const testomatioReporter = require('@testomatio/reporter/lib/adapter/cypress-plugin');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      testomatioReporter(on, config);
+
+      return config;    },
+      "baseUrl":"https://pushing-front.vercel.app/"
   },
 });
+
+
